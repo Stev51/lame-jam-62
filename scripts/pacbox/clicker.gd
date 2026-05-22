3b099863a68dbfd3fe52be26a431680cdd07bf7f
Stev51 <dinosaurrampage51@gmail.com>
2026-05-21 21:59:12 -0500

    Switched to PACBox template

---
+++ scripts/pacbox/clicker.gd
@@ -0,0 +1,62 @@
+@tool
+extends Node2D
+
+enum DisplayMode {STILL_IMAGE, ANIMATED_IMAGE, HIDDEN_IMAGE}
+
+@export var still_image: Texture2D
+@export var animated_image: SpriteFrames
+@export var display_mode: DisplayMode = DisplayMode.STILL_IMAGE
+@export var event_id: String = "DEFAULT"
+@export var flag_id: String = ""
+
+@onready var still_sprite_node = $Sprite2D
+@onready var anim_sprite_node = $AnimatedSprite2D
+
+func _ready() -> void:
+	
+	if not flag_id.is_empty():
+		if not PACBox.get_flag(flag_id):
+			visible = false
+	
+	still_sprite_node.texture = still_image
+	anim_sprite_node.sprite_frames = animated_image
+	update_display_mode(display_mode)
+
+func update_display_mode(mode: DisplayMode) -> void:
+	
+	display_mode = mode
+	
+	match mode:
+		DisplayMode.STILL_IMAGE:
+			still_sprite_node.visible = true
+			anim_sprite_node.visible = false
+		DisplayMode.ANIMATED_IMAGE:
+			still_sprite_node.visible = false
+			anim_sprite_node.visible = true
+		_:
+			still_sprite_node.visible = false
+			anim_sprite_node.visible = false
+
+func call_event() -> void:
+	PACBox.call_event(event_id)
+
+func still_img() -> void:
+	update_display_mode(DisplayMode.STILL_IMAGE)
+
+func anim_img() -> void:
+	update_display_mode(DisplayMode.ANIMATED_IMAGE)
+
+func hide_img() -> void:
+	update_display_mode(DisplayMode.HIDDEN_IMAGE)
+
+func enable() -> void:
+	visible = true
+
+func disable() -> void:
+	visible = false
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	# Left mouse button pressed
+	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
+		get_viewport().set_input_as_handled()
+		call_event()
---
+++ scripts/pacbox/pacbox.gd
@@ -0,0 +1,74 @@
+extends Node2D
+
+@onready var events = Events.new()
+@onready var flags = Flags.new()
+
+@onready var room_node = $Room
+
+func _ready() -> void:
+	
+	var viewport = get_viewport()
+	viewport.physics_object_picking_first_only = false
+	viewport.physics_object_picking_sort = true
+	
+	disable()
+
+func start(room_id: String) -> void:
+	if switch_room(room_id):
+		enable()
+	else:
+		print("ERROR: PACBox not starting (invalid room ID).")
+
+func enable() -> void:
+	visible = true
+
+func disable() -> void:
+	visible = false
+
+func call_event(id: String) -> void:
+	events.call_event(id)
+
+func get_flag(key: String) -> bool:
+	if key in flags.flags:
+		return flags.flags[key]
+	else:
+		print("ERROR: Accessed invalid story flag \"" + key + "\". Returning FALSE as a default value.")
+		return false
+
+# Returns true if flag already existed
+func set_flag(key: String, val: bool = true) -> bool:
+	var ret_val = (key in flags.flags)
+	flags.flags[key] = val
+	return ret_val
+
+func fade_to_room(id: String) -> bool:
+	
+	FadeScreen.fade_out()
+	await FadeScreen.fade_out_finished
+	
+	var result = switch_room(id)
+	
+	FadeScreen.fade_in()
+	await FadeScreen.fade_in_finished
+	
+	return result
+
+func switch_room(id: String) -> bool:
+	
+	var path_name = "res://scenes/pacbox_rooms/" + id + ".tscn"
+	
+	if not ResourceLoader.exists(path_name):
+		print("ERROR: Attempted to switch to a nonexistent room scene: " + path_name)
+		return false
+	
+	var new_room = load(path_name).instantiate()
+	
+	for child in room_node.get_children():
+		child.queue_free()
+	
+	room_node.add_child(new_room)
+	
+	return true
+
+func queue_dialog(id: String) -> void:
+	Dialogic.start(id)
