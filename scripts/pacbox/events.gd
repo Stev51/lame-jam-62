28f19d8879ba7b8ee1234d9f7c530bb9285ef31b
Stev51 <dinosaurrampage51@gmail.com>
2026-05-24 10:27:36 -0500

    Remove default PACBox stuff

--- scripts/pacbox/events.gd
+++ scripts/pacbox/events.gd
@@ -563,27 +563,6 @@ func call_event(id: String) -> void:
 			
 			PACBox.fade_to_room("room_night_hallway_1")
 			
-		"demo_event_go_room1":
-			
-			PACBox.fade_to_room("demo_room_1")
-			
-		"demo_event_go_room2":
-			
-			PACBox.fade_to_room("demo_room_2")
-			
-		"demo_event_dialog_1":
-			
-			PACBox.queue_dialog("demo_timeline_1")
-			
-		"demo_event_dialog_2":
-			
-			PACBox.set_flag("demo_flag")
-			PACBox.queue_dialog("demo_timeline_2")
-			
-		"demo_event_dialog_3":
-			
-			PACBox.queue_dialog("demo_timeline_3")
-			
 		"DEFAULT":
 			
 			print("Default event invoked! Please define a custom event ID!")
